2fadb1f7da5340a1ca20ed293dd5b6a4629fde83
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2023-01-27 17:27:12 +0100

    :white_check_mark: Add Vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import App from './App';
+import { render, screen, userEvent } from './test/test-utils';
+
+describe('App', () => {
+  it('the title is visible', () => {
+    render(<App />);
+    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+  });
+
+  it('should increment count on click', async () => {
+    render(<App />);
+    userEvent.click(screen.getByRole('button'));
+    expect(await screen.findByText(/count is 1/i)).toBeInTheDocument();
+  });
+});
