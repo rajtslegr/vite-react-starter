b16ea17755848c0610231a79bda563b048e2ab99
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2023-01-27 22:18:52 +0100

    :wrench: Edit configuration files

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  endOfLine: 'auto',
+};
