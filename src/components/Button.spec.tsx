ed8a54971e155d567371473740bfe08580502482
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2023-03-23 22:27:19 +0100

    :white_check_mark: Add test

---
+++ src/components/Button.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen, userEvent } from '@test/test-utils';
+
+import Button from './Button';
+
+describe('Button', () => {
+  it('should render the button', () => {
+    render(<Button>Click me</Button>);
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('should call the onClick handler', () => {
+    const onClick = vi.fn();
+    render(<Button onClick={onClick()}>Click me</Button>);
+    userEvent.click(screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
